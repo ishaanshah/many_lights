05127763915adbf8f4b79dc78a58df907ac60ae1
Ishaan Shah <ishaan.n.shah@gmail.com>
2023-05-12 16:16:15 +0530

    Fix bug in RIS

--- plugins/ris.py
+++ plugins/ris.py
@@ -10,7 +10,7 @@ from utils.render import render_multi_pass, linear_to_srgb
 from utils.parser import add_common_args
 from utils.reservoir import Reservoir
 
-import ipdb
+EPS = dr.epsilon(mi.Float)
 
 class ReSTIR(mi.SamplingIntegrator):
     def __init__(self, props: mi.Properties) -> None:
@@ -44,45 +44,47 @@ class ReSTIR(mi.SamplingIntegrator):
 
             # Calculate RIS weight
             p_hat = dr.norm(bsdf_val * emitter_val)
-            w = p_hat / dr.maximum(1e-6, ds.pdf)
+            w = dr.select(ds.pdf > EPS, p_hat, 0)
 
-            reservoir.update(w, ds, sampler.next_1d(active), active)
+            reservoir.update(w, ds, sampler.next_1d(active))
 
             i += 1
 
-        active = ~scene.ray_test(si.spawn_ray_to(reservoir.sample.p), active)
-        emmiter_val = scene.eval_emitter_direction(si, reservoir.sample, active)
+        intersected = ~scene.ray_test(si.spawn_ray_to(reservoir.sample.p), active)
+        emitter_val = scene.eval_emitter_direction(si, reservoir.sample, active)
         wo = si.to_local(reservoir.sample.d)
         bsdf_val = bsdf.eval(ctx, si, wo)
 
         p_hat = dr.norm(bsdf_val * emitter_val)
-        W = dr.select(p_hat > 1e-6, dr.rcp(p_hat) * reservoir.w_sum * dr.rcp(self.m), 0)
+        W = dr.select(p_hat > EPS, dr.rcp(p_hat) * reservoir.w_sum * dr.rcp(self.m), 0)
 
-        result += bsdf_val * emmiter_val * W
+        result += dr.select(intersected & active, bsdf_val * emitter_val * W, 0)
 
         return result, active, []
 
 
-mi.register_integrator("restir", lambda props: ReSTIR(props))
+if __name__ == "__main__":
+    mi.register_integrator("restir", lambda props: ReSTIR(props))
 
-parser = ArgumentParser(conflict_handler="resolve")
-add_common_args(parser)
-args = parser.parse_args()
+    parser = ArgumentParser(conflict_handler="resolve")
+    add_common_args(parser)
+    args = parser.parse_args()
 
-scene_desc = mi.cornell_box()
-scene: mi.Scene = mi.load_file("scenes/veach-mis/scene.xml")
+    scene_desc = mi.cornell_box()
+    scene: mi.Scene = mi.load_file("scenes/veach-mis/scene.xml")
+    # scene: mi.Scene = mi.load_dict(scene_desc)
 
-# Define integrators
-integrators = {
-    "restir": mi.load_dict({ "type": "restir" }),
-    "mis": mi.load_dict({ "type": "direct" }),
-    "emitter": mi.load_dict({ "type": "direct", "bsdf_samples": 0 }),
-    "bsdf": mi.load_dict({ "type": "direct", "emitter_samples": 0 })
-}
+    # Define integrators
+    integrators = {
+        "restir": mi.load_dict({ "type": "restir", "num_proposals": 32 }),
+        "mis": mi.load_dict({ "type": "direct" }),
+        "emitter": mi.load_dict({ "type": "direct", "bsdf_samples": 0 }),
+        "bsdf": mi.load_dict({ "type": "direct", "emitter_samples": 0 })
+    }
 
-for int_name, integrator in integrators.items():
-    render_func = lambda scene, seed, spp: mi.render(scene, spp=spp, integrator=integrator, seed=seed)
-    res = render_multi_pass(render_func, args.resolution, args.resolution, scene, args.spp, f"{int_name}.exr")
-    if args.show_render:
-        plt.imshow(linear_to_srgb(res))
-        plt.show()
+    for int_name, integrator in integrators.items():
+        render_func = lambda scene, seed, spp: mi.render(scene, spp=spp, integrator=integrator, seed=seed)
+        res = render_multi_pass(render_func, args.resolution, args.resolution, scene, args.spp, f"{int_name}.exr")
+        if args.show_render:
+            plt.imshow(linear_to_srgb(res))
+            plt.show()
--- utils/reservoir.py
+++ utils/reservoir.py
@@ -7,9 +7,9 @@ class Reservoir():
         self.w_sum = dr.zeros(mi.Float)
         self.dtype = dtype
     
-    def update(self, w: mi.Float, x, sample: mi.Float, active: bool=True):
+    def update(self, w: mi.Float, x, rand: mi.Float):
         self.w_sum += w
-        self.sample = dr.select((sample < w / dr.maximum(self.w_sum, 1e-6)), x, self.sample)
+        self.sample = dr.select((rand < w / self.w_sum), x, self.sample)
     
     def __repr__(self) -> str:
         return f"w_sum: {self.w_sum}, sample: {self.sample}"
